230c4be92c5c4387cafb7b346e1597cdd1c0f37a
Emily Phelps <52965134+EmilyPhelps@users.noreply.github.com>
2024-10-01 10:21:56 +0100

    Delete mito_depth.sh

--- mito_depth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BAM_ARRAY=( $(find /local/fatbob/Resequencing_Fulleri_2022/mito/remove_dups/mito/ -name '*bam') )
-GENOME=/local/fatbob/Resequencing_Fulleri_2022/mito/Corydoras_trilineatus_mito.fa
-
-for BAM in ${BAM_ARRAY[@]}; do
-  SAMPLE_NAME=$(echo $BAM | awk -F"/" '{print $(NF)}' | sed 's/_remove_dups.bam//g' )
-  samtools depth -aa  ${BAM} --reference ${GENOME} > ${SAMPLE_NAME}'_depth.txt'
-done
